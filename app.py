9cdbbba2f90df035c8cddea60febce2bfd4085ea
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-05-16 10:06:52 -0500

    Add loan calculation inputs and function call

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import libreria_funciones as lf
 
 st.title("Proyecto Final Universidad Casa Grande")
 
@@ -14,6 +15,14 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+
+monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
+
+cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+st.write("Su cuota mensual es: ",cuota
 
 
 
